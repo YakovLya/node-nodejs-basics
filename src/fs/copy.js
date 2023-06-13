bee45fdb55aefb357f250775ef94cad3b33e596a
YakovLya <yakaulyahovich@gmail.com>
2023-06-13 16:09:55 +0300

    feat: implement fs subtask

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import fs from 'fs'
+const path_from = './src/fs/files'
+const path_to = './src/fs/files_copy'
+
 const copy = async () => {
-    // Write your code here 
+    fs.access(path_from, (err) => {
+        if (err)
+            throw new Error('FS operation failed')
+        fs.access(path_to, (err) => {
+            if (err) {
+                fs.cp(path_from, path_to, { recursive: true }, (err) => {
+                    if (err)
+                        throw new Error('FS operation failed')
+                })
+            } else
+                throw new Error('FS operation failed')
+        })
+    })
 };
 
 await copy();
