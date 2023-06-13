2e09fa4e737f78931bc1bb97c7c614e2c752f4f0
YakovLya <yakaulyahovich@gmail.com>
2023-06-13 22:52:02 +0300

    feat: implement zip subtask

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from 'fs'
+import { createGunzip } from 'zlib'
+const path_in = './src/zip/files/archive.gz'
+const path_out = './src/zip/files/fileToCompress2.txt'
+
 const decompress = async () => {
-    // Write your code here 
+    const stream_in = fs.createReadStream(path_in)
+    const unzip = createGunzip()
+    const stream_out = fs.createWriteStream(path_out)
+    stream_in.pipe(unzip).pipe(stream_out)
 };
 
 await decompress();
