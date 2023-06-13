45546b3ca5058568205270d382e189e350388fa2
YakovLya <yakaulyahovich@gmail.com>
2023-06-13 18:50:23 +0300

    feat: implement cli subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    // Write your code here 
+    let out = []
+    process.argv.forEach((val, index, array) => {
+        if (index >= 2 && index % 2 == 0)
+            out.push(val.slice(2) + ' is ' + array[index + 1])
+    })
+    console.log(out.join(', '))
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    let out = []
+    for (let key in process.env)
+        if (key.startsWith('RSS_'))
+            out.push(key + '=' + process.env[key])
+    console.log(out.join('; '))
 };
 
 parseEnv();
