e99eb013fa91c509f1a5ad33c06d43e9b333d965
YakovLya <yakaulyahovich@gmail.com>
2023-06-14 22:11:25 +0300

    feat: implement wt subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker } from 'worker_threads'
+import { cpus } from 'os'
+const cpu_num = cpus().length
+const path = './src/wt/worker.js'
+const result = []
+
+function createWorker(num) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(path, {
+            workerData: num
+        })
+        worker.on('message', resolve)
+        worker.on('error', reject)
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const promises = []
+    for (let num = 0; num < cpu_num; num++) 
+        promises.push(createWorker(10 + num))
+    await Promise.allSettled(promises).then((results) => {
+        results.forEach((worker_result) => {
+            if (worker_result.status == 'fulfilled') {
+                result.push({
+                    status: 'resolved',
+                    data: worker_result.value
+                })
+            } else {
+                result.push({
+                    status: 'error',
+                    data: none
+                })
+            }
+        })
+    })
 };
 
 await performCalculations();
+console.log(result)
